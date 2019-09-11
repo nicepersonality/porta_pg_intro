6135716a58ff3f6e1c44cc958c14556c04435b21
Lilith Bentley <sbentley@gmail.com>
2019-09-11 14:26:19 -0500

    Rewrite server to use modularized route/pool.

--- server/server.js
+++ server/server.js
@@ -1,23 +1,5 @@
 const express = require('express');
 const bodyParser = require( 'body-parser' );
-const pg = require( 'pg' );
-
-const Pool = pg.Pool;
-const pool = new Pool({
-    database: 'music_library',
-    host: 'localhost',
-    port: 5432,
-    max: 10,
-    idleTimeoutMillis: 30000
-});
-
-pool.on( 'connect', () => {
-    console.log( 'Postgresql connected' );
-});
-pool.on( 'error', (error) => {
-    console.log( 'Error with Postgres pool:', error );
-});
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -25,16 +7,8 @@ app.use(express.static('server/public'));
 app.use( bodyParser.urlencoded( { extended: true } ) )
 
 
-// let musicRouter = require('./routes/music_router');
-// app.use('/musicLibrary', musicRouter);
-
-app.get('/musicLibrary', (req, res) => {
-    // res.send(musicLibrary);
-    let queryText = `SELECT * FROM "songs";`;
-    pool.query(queryText).then( (result) => {     
-        console.log('We have results!', result);
-    })
-});
+let musicRouter = require('./routes/music_router');
+app.use('/musicLibrary', musicRouter);
 
 
 app.listen(PORT, () => {
