fb813d31dcd1b1b958cd444784e8b5154156ec61
Lilith Bentley <sbentley@gmail.com>
2019-09-11 14:24:09 -0500

    Update music_router to use pool module for DB.

--- server/routes/music_router.js
+++ server/routes/music_router.js
@@ -1,48 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../modules/pool');
 
-let musicLibrary = [
-    {
-        artist: "Sturgill Simpson",
-        track: "Keep It Between the Lines",
-        rank: 1,
-        published: "4-16-2016"
-    },
-    {
-        artist: "Margo Price",
-        track: "Since You Put Me Down",
-        rank: 2,
-        published: "3-25-2016"
-    },
-    {
-        artist: "Jason Isbell",
-        track: "Alabama Pines",
-        rank: 3,
-        published: "4-12-2011"
-    },
-    {
-        artist: "Midland",
-        track: "Check Cashin' Country",
-        rank: 4,
-        published: "9-22-2017"
-    },
-    {
-        artist: "Nikki Lane",
-        track: "Highway Queen",
-        rank: 5,
-        published: "2-17-2017"
-    }
-]
-
-// router.get('/', (req, res) => {
-//     // res.send(musicLibrary);
-//     let queryText = `SELECT * FROM "songs";`;
-// });
+router.get('/', (req, res) => {
+    let queryText = `SELECT * FROM "songs";`;
+    pool.query(queryText).then( (result) => {     
+        // console.log('We have results!', result);
+        res.send(result.rows);
+    }).catch( (error) => {
+        console.log( 'Error making query:', error );
+        res.sendStatus(500);
+    });
+});
 
 router.post('/', (req, res) => {
     console.log("HELLO FROM THE POST", req.body);
-    res.sendStatus(200);
-    musicLibrary.push(req.body);
-})
+    const newSong = req.body;
+    const queryText = `INSERT INTO "songs" ("artist", "track", "rank", "published")
+    VALUES ('${newSong.artist}', '${newSong.track}', ${newSong.rank}, '${newSong.published}');`;
+    pool.query(queryText).then( (result) => {
+        console.log('Post result:', result);
+        res.sendStatus(201);
+    }).catch( (error) => {
+        console.log( 'Error making query:', error );
+        res.sendStatus(500);
+    });
+});
 
 module.exports = router;
